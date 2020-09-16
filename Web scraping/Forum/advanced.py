58c18d9ecb09cf6c99a50bebf954f0b6659678a8
Sujin Han (Susie) <56803165+hansujin93@users.noreply.github.com>
2020-09-17 00:31:38 +0900

    Update advanced.py

--- Web scraping/Forum/advanced.py
+++ Web scraping/Forum/advanced.py
@@ -9,3 +9,33 @@ ppg = soup.find('div', {'class': 'pages'}).find('p').text
 result = lastpage.search(ppg)
 last_pp = result.group(3)
 print(last_pp)
+
+
+posts = []
+lastpage = re.compile(r"page\s(\d+)(\s)of\s(\d+)")
+ppg = soup.find('div', {'class': 'pages'}).find('p').text
+lastpage_num = int(lastpage.search(ppg).group(3))
+
+startpost = 0
+for i in range(1, lastpage_num+1):
+    if i > 1:
+        startpost = 1
+    pageload = {'pg': i}
+    r = requests.get('https://www.mumsnet.com/Talk/toys_and_games_chat/3414974-noisy-baby-toys-which-are-the-worst'
+                 , params=pageload)
+    soup = BeautifulSoup(r.text, "html.parser")
+    msgs = soup.find_all('div', {'class': 'talk-post'})
+    
+    for m in range(startpost, len(msgs)):
+        for href in msgs[m].select('a[href]'):
+            href.decompose()
+        post = msgs[m].find('p')
+        for br in post.find_all("br"):
+            br.replace_with("\n")
+        plain_post = re.sub('[^a-zA-Z ,0-9]','',post.text)  
+        plain_post = plain_post.replace('  ',' ')
+        posts.append(plain_post.strip())
+
+with open('forum_result_advanced.txt', 'w') as f:
+    for pp in posts:
+        f.write("%s\n" % pp.replace("\n", "  ").replace("\r", "  "))
